540a045f7d59eca5e08ddeef75274c68b0b93ad8
Allyounna <145887785+allyounna@users.noreply.github.com>
2024-02-13 14:52:22 +0700

    Update ip_ping.py

--- 1/ip_ping.py
+++ 1/ip_ping.py
@@ -1,7 +1,9 @@
 import os
+
+
 domains = ['google.com', 'wikipedia.org', 'yandex.ru', 'nsu.ru', 'youtube.com', '2gis.ru', 'apple.com', 'cian.ru', 'habr.com', 'github.com']
-f = open('ping.cvs', 'w')
-f.write(',domain,ip,ttl\n')
+file = open('ping.cvs', 'w')
+file.write(',domain,ip,ttl\n')
 iterator = 0
 for domain in domains:
     iterator += 1
@@ -9,5 +11,5 @@ for domain in domains:
     response = os.popen(command).read().split(' ')
     ip = response[10][0:-1]
     ttl = response[14][(response[14].find('=') + 1):(response[14].find('\n'))]
-    f.write(str(iterator) + ',' + domain + ',' + ip + ',' + ttl + '\n')
+    file.write(str(iterator) + ',' + domain + ',' + ip + ',' + ttl + '\n')
 
